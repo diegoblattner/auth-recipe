a91967d06ecf41658dff4e6e63175f83c4bb4524
Diego Blattner <>
2025-08-28 10:30:14 +0100

    ignoring dist folder

--- dist/utils.js
+++
@@ -1,8 +0,0 @@
-import { readFileSync } from "node:fs";
-export function readFileAsString(path) {
-    const content = readFileSync(path).toString();
-    return content;
-}
-export function generateRandomToken(size = 6) {
-    return new Array(size).fill(0).map(() => Math.floor(Math.random() * 10)).join("");
-}
